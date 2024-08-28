b81f13abc26485ac8ce7b890bdaa00c7c9cbf59d
Chris Tolleson <christolleson@alt0fhtxrp2.mynetworksettings.com>
2024-08-28 18:19:09 -0400

    initial commit

--- tf-vcn/outputs.tf
+++ tf-vcn/outputs.tf
@@ -39,11 +39,3 @@ output "public-security-list-OCID" {
 }
 
 
-# Outputs for public security list
-
-output "public-security-list-name" {
-  value = oci_core_security_list.public-security-list.display_name
-}
-output "public-security-list-OCID" {
-  value = oci_core_security_list.public-security-list.id
-}
--- tf-vcn/public-security-list.tf
+++ tf-vcn/public-security-list.tf
@@ -45,7 +45,7 @@ ingress_security_rules {
   
   ingress_security_rules { 
       stateless = false
-      source = "10.0.0.0/16"
+      source = "10.0.0.0/0"
       source_type = "CIDR_BLOCK"
       # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
       protocol = "1"
